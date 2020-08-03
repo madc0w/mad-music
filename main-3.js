7ea489bcca42c74f270ffab6459637d5e5e43368
mad <marc@balloonary.io>
2020-08-03 09:57:17 +0200

    .

--- main-3.js
+++ main-3.js
@@ -9,7 +9,7 @@ function init() {
 	request.open('GET', 'http://heliosophiclabs.com/~mad/projects/mad-music/non.mp3', true);
 	request.responseType = 'arraybuffer';
 	request.onload = () => {
-		audioContext.decodeAudioData(request.response, buffer => {
+		audioCtx.decodeAudioData(request.response, buffer => {
 			buff = buffer;
 		}, err => {
 			console.error(err);
